9c456e2c317c9ad04a9079a88c495e32bea8b46c
Jose <jsimalsanmartin@danielcastelao.org>
2023-10-02 08:59:38 +0200

    Boletin02

---
+++ Boletin02/Boletin02_1/boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin2_1;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       float base= 4,altura= 3,area;
+       area = base*altura/2;
+       System.out.println("area do triangulo = "+area);
+       
+        // TODO code application logic here
+    }
+    
+}
